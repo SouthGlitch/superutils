eb383d6efcb93581ae65f1d5380ca2e1122bc701
nahuel <nahuelrabeywork@gmail.com>
2021-09-06 15:51:59 -0300

    empty Functions

--- src/Functions.ts
+++ src/Functions.ts
@@ -25,37 +25,3 @@
 // 	);
 // }
 
-type message_parser<T> = (v: T) => string;
-type parserHash = {
-  [name: string]: message_parser<any>;
-};
-const parserFunctions: parserHash = {
-  boolean: (v: boolean) => "(boolean)" + String(v),
-  number: (v: number) => "(number)"+ String(v),
-  string: (v: string) => "(number)"+v,
-  undefined: (v: undefined) => "(undefined)",
-  symbol: (v: symbol) => "(symbol)",
-  function: (v: Function) => "(function)",
-  object: (v: object) => "\n\t"+inspect(v)
-};
-
-function inspect(item: any): string {
-  let message = "";
-  for (const key in item) {
-    const key_value = item[key];
-	message += `${key}: ${parserFunctions[typeof key_value]}\n`;
-  }
-  return message;
-}
-
-function test(){
-	const test = {
-		foo: "bar",
-		baz: true,
-		laz: 2,
-		mix: undefined,
-		tes: Symbol("test"),
-		cos: () => {},
-	}
-	console.log(inspect(test));
-}
