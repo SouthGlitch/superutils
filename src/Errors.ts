e8d096f969265ca2ae525a062b186933a205888c
nahuel <nahuelrabeywork@gmail.com>
2021-09-06 15:56:51 -0300

    commented "bad" errors

--- src/Errors.ts
+++ src/Errors.ts
@@ -2,7 +2,7 @@ import { inspect } from "./inspect";
 type references = {
   [name: string]: any;
 };
-abstract class SuperError extends Error {
+export abstract class SuperError extends Error {
   references!: references;
   /**
    * Get a references string
@@ -21,22 +21,22 @@ abstract class SuperError extends Error {
   abstract console_log(): void;
 }
 
-export class BugAlert extends Error {
-  public usefullData: any;
-  constructor(usefullData: any) {
-    let message = "Atention! There was a bug, prepare de debbuger!\n";
-    message += `usefull data: \n${JSON.stringify(usefullData, null, 2)}`;
-    super(message);
-    this.name = "BugAlert";
-    this.usefullData = usefullData;
-  }
-}
+// export class BugAlert extends Error {
+//   public usefullData: any;
+//   constructor(usefullData: any) {
+//     let message = "Atention! There was a bug, prepare de debbuger!\n";
+//     message += `usefull data: \n${JSON.stringify(usefullData, null, 2)}`;
+//     super(message);
+//     this.name = "BugAlert";
+//     this.usefullData = usefullData;
+//   }
+// }
 
-export class LostObject extends Error {
-  constructor(slug: string) {
-    const message = `${slug} no se encunetra en la base de datos`;
-    super(message);
-    this.message = message;
-    this.name = "LostObject";
-  }
-}
+// export class LostObject extends Error {
+//   constructor(slug: string) {
+//     const message = `${slug} no se encunetra en la base de datos`;
+//     super(message);
+//     this.message = message;
+//     this.name = "LostObject";
+//   }
+// }
