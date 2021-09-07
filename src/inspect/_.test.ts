74f9ff77b9c5ef2c1b93053cb2460a00376a892c
nahuel <nahuelrabeywork@gmail.com>
2021-09-06 21:55:23 -0300

    created some test with classes for inspect function

--- src/inspect/_.test.ts
+++ src/inspect/_.test.ts
@@ -1,6 +1,6 @@
 import { inspect } from "./index";
 
-describe("inspect unit testing", () => {
+describe("plain objects", () => {
   const obj = {
     foo: "bar",
     baz: true,
@@ -9,14 +9,29 @@ describe("inspect unit testing", () => {
     tes: Symbol("test"),
     cos: () => {},
   };
-  test("plain object",()=>{
+  test("no nesting", () => {
     const msg = inspect(obj);
     console.log(msg);
     expect(msg).not.toBeUndefined();
   });
-  test("nested object",()=>{
-    const msg = inspect({...obj, obj});
+  test("nested object", () => {
+    const msg = inspect({ ...obj, obj });
     console.log(msg);
     expect(msg).not.toBeUndefined();
   });
+});
+
+describe("classes", () => {
+  class Simple {
+    field1: string = "a";
+    field2: number = 3;
+    field3: boolean = false;
+  }
+
+  test("simple class", ()=>{
+    const instance = new Simple();
+    const msg = inspect(instance);
+    console.log(msg);
+    expect(msg).not.toBeUndefined();
+  })
 });
